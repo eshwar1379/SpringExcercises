7e10dfee14816278269187dd15cb241b12655b5b
Eshwar <eshwarerukulla1@gmail.com>
2023-09-18 21:00:20 +0530

    First commit

---
+++ src/main/java/com/example/firstofspring/FirstOfSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstofspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstOfSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstOfSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstofspring/FirstOfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstofspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstOfSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
